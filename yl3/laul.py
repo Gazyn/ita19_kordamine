74ae21c45a5da82e31b6c07f9c72623edf788a37
Gazyn <jurgen.sepp@khk.ee>
2020-09-07 12:31:13 +0100

    On koostatud ja testitud laulu klass

---
+++ yl3/laul.py
@@ -0,0 +1,10 @@
+class Laul:
+    """klass Laul
+
+    Klassi omadused: (attributes)
+    pealkiri(str) - laulu pealkiri (title)
+    laulja(str) - laulu laulja (artist)
+    """
+    def __init__(self, pealkiri, laulja):
+        self.pealkiri = pealkiri
+        self.laulja = laulja
---
+++ yl3/menüü.py
@@ -0,0 +1,4 @@
+from laul import Laul
+# testime laulu objekti
+test = Laul("Test Pealkiri", "Test Laulja")
+print(test.pealkiri, test.laulja)
